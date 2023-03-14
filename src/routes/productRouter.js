3df132912ec6c8e03c5dd05a6d4b2f3e90825615
mateussousaa <mateussousa20@outlook.com>
2023-03-14 18:23:11 -0300

    feat: creating the product validation with middleware- [validadeProduct]

--- src/routes/productRouter.js
+++ src/routes/productRouter.js
@@ -1,8 +1,9 @@
 import express from "express";
 import * as productController from '../controllers/productController.js'
+import { validateProduct } from "../middlewares/validateProduct.js";
 
 const productRouter = express.Router();
 
-productRouter.post('/', productController.insertProduct)
+productRouter.post('/', validateProduct, productController.insertProduct)
 
 export { productRouter };
