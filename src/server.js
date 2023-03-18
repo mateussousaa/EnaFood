d486f917d38e7f10c370692d8c3abdbd77a281ba
mateussousaa <mateussousa20@outlook.com>
2023-03-17 21:03:46 -0300

    feat: creating the productSeeder to starts with 10 products in DB

--- src/server.js
+++ src/server.js
@@ -1,9 +1,16 @@
 import { app } from './app.js';
 import { connectToDB } from './database/connections.js';
+import productModel from './models/Product.js';
+import { productSeeder } from './utils/productSeeder.js';
 
 connectToDB()
   .then(() => {
-    app.listen(3001, () => console.log('using the port 3001 to Enafood API'));
+    app.listen(3001, () => {
+      console.log('using the port 3001 to Enafood API');
+      productModel.find({}).then((result) => {
+        if(!result.length) productModel.insertMany(productSeeder);
+      });
+    });
   })
   .catch((e) => {
     console.log('connection error with DB\n');
---
+++ src/utils/productSeeder.js
@@ -0,0 +1,14 @@
+const productSeeder = [
+  { name: 'Pizza', price: 20 },
+  { name: 'Hambúrguer', price: 15 },
+  { name: 'Sushi', price: 30 },
+  { name: 'Churrasco', price: 25 },
+  { name: 'Salada', price: 10 },
+  { name: 'Lasanha', price: 18 },
+  { name: 'Taco', price: 12 },
+  { name: 'Frango assado', price: 22 },
+  { name: 'Batata frita', price: 8 },
+  { name: 'Sopa', price: 7 }
+];
+
+export { productSeeder };
